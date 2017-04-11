d989037972bb99f538586db2f27305c421ca036b
Forest Newark <forest.newark@gmail.com>
2017-04-11 14:34:05 -0400

    initial commit

---
+++ src/main/java/com/forestnewark/SpringLoginDemoApplication.java
@@ -0,0 +1,12 @@
+package com.forestnewark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginDemoApplication.class, args);
+	}
+}
